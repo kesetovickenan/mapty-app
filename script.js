0f59a4da25e05ce54de32316fc43061d78b06a93
KesetovicKenan <kenanhoi@hotmail.com>
2024-09-19 18:53:16 +0200

    Added geolocation and coordinates

--- script.js
+++ script.js
@@ -19,6 +19,20 @@ if (navigator.geolocation)
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},13z?entry=ttu&g_ep=EgoyMDI0MDkxNS4wIKXMDSoASAFQAw%3D%3D`
       );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
